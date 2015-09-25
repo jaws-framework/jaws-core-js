1e6f09636bfd17f2d56ad6c0361705750851e008
Ryan Pendergast <ryanp@doapps.com>
2015-09-25 16:26:02 -0500

    fix for local testing and multi level deploys

--- env/index.js
+++ env/index.js
@@ -1,3 +1,31 @@
 'use strict';
 
-require('dotenv').load();
+/**
+ * Find closest .env file. Need to do this because .env is not always at root of project (like during local testing)
+ */
+var path = require('path'),
+    fs   = require('fs');
+
+function fileExistsSync(path) {
+  try {
+    var stats = fs.lstatSync(path);
+    return stats.isFile();
+  }
+  catch (e) {
+    return false;
+  }
+}
+
+var dirName = eval('__dirname'),
+    i       = 0;
+
+while (i < 6) {
+  if (fileExistsSync(path.join(dirName, '.env'))) {
+    break;
+  }
+
+  dirName = path.join(dirName, '..');
+  ++i;
+}
+
+require('dotenv').config({path: path.join(dirName, '.env'), silent: true});
